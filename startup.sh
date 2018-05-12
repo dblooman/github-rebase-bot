ad582a9b12e01bbf738f1844d0b454a8f9045960
DaveBlooman <david.blooman@gmail.com>
2018-05-12 17:38:59 +0100

    use fixed dns

--- startup.sh
+++ startup.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-function get_public_dns() {
-  curl -s -ik -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" https://kubernetes.default.svc.cluster.local/api/v1/namespaces/github/services/rebase-bot | grep "ip" | awk '{print $2}' | awk -F'"' '{print $2}'
-}
-
-# only determine k8s IP if not set explicitly
-if [[ "z$PUBLIC_DNS" == "z" ]]; then
-  PUBLIC_DNS=$(get_public_dns)
-  while [[ "z$PUBLIC_DNS" == "z" ]]; do
-    echo "Waiting for service to come up"
-    sleep 2
-    PUBLIC_DNS=$(get_public_dns)
-  done
-fi
-
 /github-rebase-bot \
  -repos "$GITHUB_REPOS" \
  -public-dns http://$PUBLIC_DNS \
